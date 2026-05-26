11c5bdc7a5b7e9727d7792f55452de47e871478d
Sapucaia Milena, (Milena.Sapucaia@ctw.bmwgroup.com) <Milena.Sapucaia+bmw@ctw.bmwgroup.com>
2026-05-26 20:11:03 +0100

    Defined typescript and removed root files not being used

--- frontend/i18n.ts
+++ frontend/i18n.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-import en from './locales/en.json'
-import pt from './locales/pt.json'
+import en from './src/locales/en.json'
+import pt from './src/locales/pt.json'
 
 i18n
   .use(LanguageDetector)
