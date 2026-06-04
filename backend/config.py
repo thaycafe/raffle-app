b3760a4ea594bf13004ff9e575343a0fd955212c
Thaynara Mendes <thaynaramendss@gmail.com>
2026-06-04 15:47:17 +0100

    feat: add draw endpoint that result the winner details

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,4 @@
-from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Prize(BaseModel):
-    title: str
-    description: str = ""
-    position: int = 1
 
 
 class Settings(BaseSettings):
@@ -17,9 +10,6 @@ class Settings(BaseSettings):
     )
 
     raffle_title: str = "Raffle App"
-    raffle_prizes: list[Prize] = [
-        Prize(title="Mistery Prize", description="A surprise gift!", position=1)
-    ]
     raffle_total_numbers: int = 100
     raffle_price: float = 10.00
     raffle_currency: str = "EUR"
